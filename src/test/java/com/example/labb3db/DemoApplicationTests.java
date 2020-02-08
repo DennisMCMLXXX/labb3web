dcef52bade57d8f65fe0d068096fbb5525546257
Dennis <tryckare@gmail.com>
2020-02-08 11:00:52 +0100

    initial commit

---
+++ src/test/java/com/example/labb3db/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labb3db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
