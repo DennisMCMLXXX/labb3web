59fa56d7e360abbd26903fd7dc240efeb7a98467
Dennis <tryckare@gmail.com>
2020-02-08 11:51:50 +0100

    utv pågår

--- src/main/java/com/example/labb3db/DemoApplication.java
+++ src/main/java/com/example/labb3db/DemoApplication.java
@@ -1,5 +1,11 @@
 package com.example.labb3db;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 
 import org.springframework.boot.SpringApplication;
@@ -22,5 +28,27 @@ public class DemoApplication {
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@RequestMapping("/filewrite")
+    @ResponseBody
+    String filewrite() {
+        int nbr = 0;
+        try {
+            File file = new File("counter.txt");
+            if (!file.exists()) {
+                file.createNewFile();}
+            else {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String oldNbr = br.readLine();
+                nbr = Integer.parseInt(oldNbr);
+                br.close();}
+            nbr++;
+            String newNbr = "" + nbr;
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(newNbr);
+            bw.close();
+        } catch (IOException e) {return e.getMessage();}
+        return "Denna sida har laddats " + nbr + " gånger.";
+    }
 
 }
