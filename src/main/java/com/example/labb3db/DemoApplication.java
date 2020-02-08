b5868aa3c87701cd7d9631b62eca8376d731e4d7
Dennis <tryckare@gmail.com>
2020-02-08 12:30:25 +0100

    utv pågår

--- src/main/java/com/example/labb3db/DemoApplication.java
+++ src/main/java/com/example/labb3db/DemoApplication.java
@@ -10,52 +10,59 @@ import java.sql.Connection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 	@RequestMapping("/db")
-    @ResponseBody
-    String connect() {
-        try {
-            Connection conn = DBManager.getConnection();
-            return "Success!";
-        } catch (Exception e) {return e.getMessage();}
-    }
+	@ResponseBody
+	String connect() {
+		try {
+			Connection conn = DBManager.getConnection();
+			return "Success!";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
 
-    @RequestMapping("/")
-    @ResponseBody
-        ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("index");
-        System.out.println("Skriver till konsol...");
-            return modelAndView;
-    }
+	@RequestMapping("/")
+	@ResponseBody
+	ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		System.out.println("Skriver till konsol...");
+		return modelAndView;
+	}
 
-    @RequestMapping("/filewrite")
-    @ResponseBody
-    String filewrite() {
-        int nbr = 0;
-        try {
-            File file = new File("counter.txt");
-            if (!file.exists()) {
-                file.createNewFile();}
-            else {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                String oldNbr = br.readLine();
-                nbr = Integer.parseInt(oldNbr);
-                br.close();}
-            nbr++;
-            String newNbr = "" + nbr;
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(newNbr);
-            bw.close();
-        } catch (IOException e) {return e.getMessage();}
-        return "Denna sida har laddats " + nbr + " gånger.";
-    }
+	@RequestMapping("/filewrite")
+	@ResponseBody
+	String filewrite() {
+		int nbr = 0;
+		try {
+			File file = new File("counter.txt");
+			if (!file.exists()) {
+				file.createNewFile();
+			} else {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String oldNbr = br.readLine();
+				nbr = Integer.parseInt(oldNbr);
+				br.close();
+			}
+			nbr++;
+			String newNbr = "" + nbr;
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(newNbr);
+			bw.close();
+		} catch (IOException e) {
+			return e.getMessage();
+		}
+		return "Denna sida har laddats " + nbr + " gånger.";
+	}
 
 	public static void main(String[] args) {
 
