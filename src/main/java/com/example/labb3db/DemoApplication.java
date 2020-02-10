189ebfc16fd61250159a137fc16d2b79c63a61ba
Dennis <tryckare@gmail.com>
2020-02-10 22:16:40 +0100

    utv pågår

--- src/main/java/com/example/labb3db/DemoApplication.java
+++ src/main/java/com/example/labb3db/DemoApplication.java
@@ -64,7 +64,7 @@ public class DemoApplication {
 	String DBDeletes(Model model, @PathParam("id") String id) throws URISyntaxException, SQLException {
 		ArrayList<User> output = new ArrayList<User>();
 		Connection conn = DBManager.getConnection();
-		output = DBManager.selectQuery(conn, "SELECT * FROM ppl WHERE id=" + id);
+		output = DBManager.selectQuery(conn, "SELECT * FROM ppl");
 		model.addAttribute("users", output);
 
 		return "db";
