cae1bd46d5209944571158ded1e7729bb4de6f74
Dennis <tryckare@gmail.com>
2020-02-12 21:56:05 +0100

    utv pågår

--- src/main/java/com/example/db/UserService.java
+++ src/main/java/com/example/db/UserService.java
@@ -15,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,15 +64,29 @@ public class UserService {
 		return "db_plain";
 	}
 
+	@GetMapping(value="/user/{id}", produces = MediaType.APPLICATION_JSON)
+	String getUserByid(Model model, @PathVariable("id") int id) throws SQLException, URISyntaxException {
+		ArrayList<User> output = new ArrayList<User>();
+		Connection conn = DBManager.getConnection();
+		output = DBManager.selectQuery(conn, "SELECT * FROM ppl WHERE id='" + id + "'");
+		model.addAttribute("users", output);
+		return "db_plain";
+	}
+	
 	@GET
 	@Path("/users/{idnumber}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<User> getUserByidnumber(@PathParam("idnumber") int idnumber) throws SQLException, URISyntaxException {
-		String dBName = "labb2";
-		if (sqlHandler.getUsers(dBName, idnumber)) {
-			return Processor.getUserList();
-		}
-		return null;
+	String getUserByidnumber(Model model, @PathVariable("idnumber") int idnumber) throws SQLException, URISyntaxException {
+//		String dBName = "labb2";
+//		if (sqlHandler.getUsers(dBName, idnumber)) {
+//			return Processor.getUserList();
+//		}
+//		return null;
+		ArrayList<User> output = new ArrayList<User>();
+		Connection conn = DBManager.getConnection();
+		output = DBManager.selectQuery(conn, "SELECT * FROM ppl WHERE id='" + idnumber + "'");
+		model.addAttribute("users", output);
+		return "db_plain";
 	}
 	/**
 	 * kunde inte använda mig av denna tjänst för jag fick felmeddelande att denna
