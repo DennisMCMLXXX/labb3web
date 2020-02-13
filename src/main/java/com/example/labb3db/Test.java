a8da426ceb9bbcabfbd3e05b540455ac25d38d99
Dennis <tryckare@gmail.com>
2020-02-13 22:24:11 +0100

    utv pågår

--- src/main/java/com/example/labb3db/Test.java
+++ src/main/java/com/example/labb3db/Test.java
@@ -1,13 +1,17 @@
 package com.example.labb3db;
 
 import java.net.URISyntaxException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.db.DBManager;
 import com.example.entity.User;
 
 @RequestMapping("/dbdb")
@@ -17,18 +21,22 @@ public class Test {
 	
 	@GetMapping("/3")
 //	@Produces(MediaType.APPLICATION_JSON)
-	public User getUserByidnumber2() throws SQLException, URISyntaxException {
-		if (processor.getUsers0()) {
-//			return Processor.getUserList();
-			return Processor.getUserList2();
-		}
+	public String getUserByidnumber2(Model model) throws SQLException, URISyntaxException {
+//		if (processor.getUsers0()) {
+////			return Processor.getUserList();
+//			return Processor.getUserList2();
+//		}
 //		JSONObject obj = new JSONObject(); 
 //		
 //		String name = "name";
 //		String profession = "profession";
 //		obj.put(name, "Dennis");
 //		obj.put(profession, "profession");
-		
-		return null;
+		ArrayList<User> output = new ArrayList<User>();
+		Connection conn = DBManager.getConnection();
+		output = DBManager.selectQuery(conn, "SELECT * FROM ppl;");
+		model.addAttribute("users", output);
+
+		return "db_plain";
 	}
 }
